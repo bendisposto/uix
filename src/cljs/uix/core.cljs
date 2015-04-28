5f251badf91305cdda8d9578c1e874b5763bf400
Jens Bendisposto <jens@bendisposto.de>
2015-04-28 19:54:30 +0200

    move around

--- src/cljs/uix/core.cljs
+++ src/cljs/uix/core.cljs
@@ -25,7 +25,7 @@
 
 (def ui-state
   (atom
-   {:current [:animators 0 1] #_[:editors 0 0]
+   {:current [:editors 0 0]
     :views {:editors [[:1 :2]
                       [:3 :4 :5]]
             :animators [[:6 :7]]}
@@ -39,49 +39,78 @@
             :7 {:editor [1 0] :model "animator0" :trace "212" :type :events-view}
             }}))
 
+(defn ^:extern right []
+  (let [[page row col] (:current @ui-state)
+        r-size (count (get-in @ui-state [:views page row]))]
+    (swap! ui-state assoc :current [page row (mod (inc col) r-size)])))
+
+(defn ^:extern left []
+  (let [[page row col] (:current @ui-state)
+        r-size (count (get-in @ui-state [:views page row]))]
+    (swap! ui-state assoc :current [page row (mod (dec col) r-size)])))
+
+(defn ^:extern down []
+  (let [[page row col] (:current @ui-state)
+        row-cnt (count (get-in @ui-state [:views page]))
+        next-row-number (mod (inc row) row-cnt)
+        r-size (count (get-in @ui-state [:views page next-row-number]))
+        n-col (min col (dec r-size))]
+    (.log js/console "c " col " nc " n-col " nr " next-row-number " rs " r-size)
+    (swap! ui-state assoc :current [page next-row-number n-col])))
+
+(defn ^:extern up []
+  (let [[page row col] (:current @ui-state)
+        row-cnt (count (get-in @ui-state [:views page]))
+        next-row-number (mod (dec row) row-cnt)
+        r-size (count (get-in @ui-state [:views page next-row-number]))
+        n-col (min col (dec r-size))]
+    (.log js/console "c " col " nc " n-col " nr " next-row-number " rs " r-size)
+    (swap! ui-state assoc :current [page next-row-number n-col])))
+
 
 (defn show [page row column]
   (let [c (:current @ui-state)]
     (cond
       (= 3 (count c)) (when-not (= c [page row column]) {:class "hidden"})
-      :otherwise {:class "thumbnail"})))
+      :otherwise {:class "col-lg-1"})))
 
 (defmulti render-view :type)
 (defmethod render-view :editor [{:keys [formalism file row column]}]
-  [:div.editor
-   (show :editors row column)
+  [:div.editor (merge  {:key (str "ed" row column)}
+                       (show :editors row column))
    (str "Editor: " file "@"row ","column)])
 
 (defmethod render-view :state-view [{:keys [model trace row column]}]
-  [:div.state-view
-   (show :animators row column)
+  [:div.state-view (merge {:key (str "ani" row column)}
+                          (show :animators row column))
    (str "State: " model "/" trace "@"row ","column)])
 
 (defmethod render-view :events-view [{:keys [model trace row column]}]
-  [:div.events-view
-   (show :animators row column)
+  [:div.events-view (merge {:key (str "ani" row column)}
+                           (show :animators row column))
    (str "Events: " model "/" trace "@"row ","column)])
 
 (defmethod render-view :history-view [{:keys [model trace row column]}]
-  [:div.history-view
-   (show :animators row column)
+  [:div.history-view (merge {:key (str "ani" row column)}
+                            (show :animators row column))
    (str "History: " model "/" trace "@"row ","column)])
 
 
 (defn render-row [y columns]
-  [:div.row (map-indexed (fn [x key] (render-view (assoc (get-in @ui-state [:pages key]) :column x :row y))) columns)])
+  [:div.row {:key (str "row" y)}
+   (doall (map-indexed (fn [x key] (render-view (assoc (get-in @ui-state [:pages key]) :column x :row y))) columns))])
 
 (defn render-page [[section rows]]
-  [:div.page
-   (when-not (= section (first (:current @ui-state))) {:class "hidden"})
-   (map-indexed render-row rows)
+  [:div.page (merge {:key section}
+                    (when-not (= section (first (:current @ui-state))) {:class "hidden"}))
+   (doall (map-indexed render-row rows))
    ])
 
 
 (defn home-page []
   (let [views (:views @ui-state)]
     [:div.ui
-     (map render-page views)]))
+     (doall (map render-page views))]))
 
 
 ;; -------------------------
